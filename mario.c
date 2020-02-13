1677d74630f06f56c7726a24d0ab3b42acc3a020
asankov <asankov@users.noreply.github.com>
2020-02-13 20:16:10 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

---
+++ mario.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int number;
+    do {
+        number = get_int("Height: ");
+    } while (number < 1 || number > 8);
+
+    for (int i = 1; i <= number; i++) 
+    {
+        for (int j = 0; j < number - i; j++) 
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < i; j++) 
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int j = 0; j < i; j++) 
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+
+}
