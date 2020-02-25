a4d7457600211c241f2ba1c8f3622fc0c662f290
asankov <asankov@users.noreply.github.com>
2020-02-25 21:28:36 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -175,7 +175,10 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    // TODO
+    for (int i = 0; i < pair_count; i++)
+    {
+        locked[pairs[i].winner][pairs[i].loser] = true;
+    }
     return;
 }
 
