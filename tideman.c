b9b529e11860daa1772121cb95887b78847401a5
asankov <asankov@users.noreply.github.com>
2020-02-25 22:01:57 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -115,9 +115,9 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    for (int i = 0; i < candidate_count-1; i++)
+    for (int i = 0; i < candidate_count - 1; i++)
     {
-        for (int j = i+1; j < candidate_count; j++)
+        for (int j = i + 1; j < candidate_count; j++)
         {
             preferences[ranks[i]][ranks[j]]++;
         }
