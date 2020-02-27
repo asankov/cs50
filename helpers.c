433b6f0e644bbe83ae9ebe49c72716bb1641ad9e
asankov <asankov@users.noreply.github.com>
2020-02-27 21:17:34 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -10,12 +10,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             RGBTRIPLE img = image[i][j];
-            int sum = img.rgbtBlue + img.rgbtGreen + img.rgbtRed;
+            int sum = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed;
             float res = (float) sum / 3.0;
             int ress = (int) roundf(res);
-            img.rgbtBlue = ress;
-            img.rgbtGreen = ress;
-            img.rgbtRed = ress;
+            image[i][j].rgbtBlue = ress;
+            image[i][j].rgbtGreen = ress;
+            image[i][j].rgbtRed = ress;
         }
     }
 }
