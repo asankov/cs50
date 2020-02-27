6b74e84a746d55d90f6b7b6f11b20e09b3f39d2e
asankov <asankov@users.noreply.github.com>
2020-02-27 21:05:44 +0000

    automated commit by check50 [check50=True]

---
+++ helpers.c
@@ -0,0 +1,37 @@
+#include "helpers.h"
+#include <stdio.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE img = image[i][j];
+            float sum = img.rgbtBlue + img.rgbtGreen + img.rgbtRed;
+            float res = sum / 3;
+            image[i][j].rgbtBlue = res;
+            image[i][j].rgbtGreen = res;
+            image[i][j].rgbtRed = res;
+        }
+    }
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
