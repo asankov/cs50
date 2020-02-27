7f67956924735b6bf06a5bdd066324c40076c42d
asankov <asankov@users.noreply.github.com>
2020-02-27 22:29:40 +0000

    automated commit by check50 [check50=True]

---
+++ helpers.c
@@ -0,0 +1,151 @@
+#include "helpers.h"
+#include <stdio.h>
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE *img = &image[i][j];
+            if (img->rgbtBlue == img->rgbtGreen && img->rgbtGreen == img->rgbtRed)
+            {
+                continue;
+            }
+            int sum = img->rgbtBlue + img->rgbtGreen + img->rgbtRed;
+            float res = (float) sum / 3.0;
+            int ress = (int) roundf(res);
+            img->rgbtBlue = ress;
+            img->rgbtGreen = ress;
+            img->rgbtRed = ress;
+        }
+    }
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - (j + 1)];
+            image[i][width - (j + 1)] = temp;
+        }
+    }
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE newImage[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+
+            // itself
+            int aBlue = image[i][j].rgbtBlue;
+            int aRed = image[i][j].rgbtRed;
+            int aGreen = image[i][j].rgbtGreen;
+            int c = 1;
+
+            if (i != 0)
+            {
+                // top
+                aBlue += image[i - 1][j].rgbtBlue;
+                aRed += image[i - 1][j].rgbtRed;
+                aGreen += image[i - 1][j].rgbtGreen;
+                c++;
+
+
+                // top-right
+                if (j != width - 1)
+                {
+                    aBlue += image[i - 1][j + 1].rgbtBlue;
+                    aRed += image[i - 1][j + 1].rgbtRed;
+                    aGreen += image[i - 1][j + 1].rgbtGreen;
+                    c++;
+                }
+
+                // top-left
+                if (j != 0)
+                {
+                    aBlue += image[i - 1][j - 1].rgbtBlue;
+                    aRed += image[i - 1][j - 1].rgbtRed;
+                    aGreen += image[i - 1][j - 1].rgbtGreen;
+                    c++;
+                }
+            }
+
+            // right
+            if (j != width - 1)
+            {
+                aBlue += image[i][j + 1].rgbtBlue;
+                aRed += image[i][j + 1].rgbtRed;
+                aGreen += image[i][j + 1].rgbtGreen;
+                c++;
+            }
+
+            // left
+            if (j != 0)
+            {
+                aBlue += image[i][j - 1].rgbtBlue;
+                aRed += image[i][j - 1].rgbtRed;
+                aGreen += image[i][j - 1].rgbtGreen;
+                c++;
+            }
+
+            if (i != height - 1)
+            {
+                // bottom
+                aBlue += image[i + 1][j].rgbtBlue;
+                aRed += image[i + 1][j].rgbtRed;
+                aGreen += image[i + 1][j].rgbtGreen;
+                c++;
+
+                // bottom-right
+                if (j != width - 1)
+                {
+                    aBlue += image[i + 1][j + 1].rgbtBlue;
+                    aRed += image[i + 1][j + 1].rgbtRed;
+                    aGreen += image[i + 1][j + 1].rgbtGreen;
+                    c++;
+                }
+                // botoom-left
+                if (j != 0)
+                {
+                    aBlue += image[i + 1][j - 1].rgbtBlue;
+                    aRed += image[i + 1][j - 1].rgbtRed;
+                    aGreen += image[i + 1][j - 1].rgbtGreen;
+                    c++;
+                }
+            }
+
+            RGBTRIPLE *img = &image[i][j];
+
+            newImage[i][j].rgbtBlue = (int) roundf((float) aBlue / (float) c);
+            newImage[i][j].rgbtRed = (int) roundf((float)aRed / (float) c);
+            newImage[i][j].rgbtGreen = (int) roundf((float) aGreen / (float) c);
+        }
+    }
+
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+            image[i][j] = newImage[i][j];
+        }
+    }
+}
+
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
