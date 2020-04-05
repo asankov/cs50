f6e45a46d735705fb04970775a2bc0531aac697d
asankov <asankov@users.noreply.github.com>
2020-04-05 21:57:24 +0000

    automated commit by submit50

---
+++ mario.py
@@ -0,0 +1,28 @@
+
+def main():
+    n = get_number(1, 8)
+    
+    for i in range(n):
+        for j in range(n-i):
+            print(" ", end="")
+        
+        print("#"*(i+1), end="")
+        
+        print(" ", end="")
+        
+        print("#"*(i+1), end="")
+
+            
+        print()
+
+
+def get_number(min, max):
+    n = min-1
+    while n < min or n > max:
+        try:
+            n = int(input("Height: "))
+        except ValueError:
+            continue
+    return n
+    
+main()
