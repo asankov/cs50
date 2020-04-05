4902ddb45b0e34733b634def83735f8aeaa6d4dd
asankov <asankov@users.noreply.github.com>
2020-04-05 22:51:49 +0000

    automated commit by submit50

--- readability.py
+++ readability.py
@@ -3,18 +3,19 @@ text = input("Text: ")
 letters, words, sentences = 0, 1, 0
 
 for c in text:
-    if c == " ":
+    if c.isalpha():
+        letters += 1
+    elif c == " ":
         words += 1
     elif c in [".", "!", "?"]:
         sentences += 1
-    else:
-        letters += 1
 
 coef = 100 / words 
 l = coef * letters
 s = coef * sentences
 
 index = 0.0588 * l - 0.296 * s - 15.8
+print(f"coef={coef}, l={l}, s={s}, index={index}")
 
 if index > 16:
     print("Grade 16+")
