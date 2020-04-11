8032dc4209a79012da872f92b019404cb000db00
asankov <asankov@users.noreply.github.com>
2020-04-11 10:55:32 +0000

    automated commit by submit50

---
+++ dna.py
@@ -0,0 +1,33 @@
+import sys
+import csv
+
+def main():
+    if len(sys.argv) != 3:
+        print("Invalid number of arguments.")
+        return
+    
+    dataset, dna_sequence = sys.argv[1], sys.argv[2]
+    
+    with open(dataset, newline='') as csvfile:
+        with open(dna_sequence, newline='') as dnafile:
+            dna = dnafile.readlines()[0]
+
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            found = True
+            for key, value in row.items():
+                if key == 'name':
+                    name = value
+                    continue
+                
+                if dna.find(key * int(value)) == -1:
+                    found = False
+            
+            if found:
+                print(name)
+                return
+
+        print("Not found.")
+    
+main()
+    
