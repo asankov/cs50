83949bee594951bc63bdcae0ad10502ef46b9afc
asankov <asankov@users.noreply.github.com>
2020-05-12 20:03:28 +0000

    automated commit by submit50

--- 9.sql
+++ 9.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people where id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT id FROM movies where year = 2004));
+SELECT name FROM people where id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT id FROM movies where year = 2004)) ORDER BY birth;
