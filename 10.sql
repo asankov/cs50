198600983a8431eaeae6e05acf5d7768684b0be4
asankov <asankov@users.noreply.github.com>
2020-05-12 20:05:29 +0000

    automated commit by submit50

--- 10.sql
+++ 10.sql
@@ -1,1 +1,1 @@
-SELECT name, COUNT(*) FROM people WHERE id IN (SELECT person_id FROM directors WHERE movie_id IN (SELECT id FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE rating >= 9)));
+SELECT name FROM people WHERE id IN (SELECT person_id FROM directors WHERE movie_id IN (SELECT id FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE rating >= 9)));
