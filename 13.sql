39367b6db561b354dd6eff833ab684b5c39f3c64
asankov <asankov@users.noreply.github.com>
2020-05-12 20:03:28 +0000

    automated commit by submit50

--- 13.sql
+++ 13.sql
@@ -1,1 +1,1 @@
-SELECT name, COUNT(*) FROM people WHERE NAME != "Kevin Bacon" AND id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958)));
+SELECT name FROM people WHERE NAME != "Kevin Bacon" AND id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = 1958)));
--- 5.sql
+++ 5.sql
@@ -1,1 +1,1 @@
-SELECT title, year FROM movies WHERE title LIKE "Harry Potter%";
+SELECT title, year FROM movies WHERE title LIKE "Harry Potter%" ORDER BY year ASC;
