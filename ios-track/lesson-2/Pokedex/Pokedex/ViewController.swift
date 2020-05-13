c6ce0259f4e074ec1e419d249df68b966e2ed568
asankov <a.sankov96@gmail.com>
2020-05-13 23:15:03 +0300

    iOS Track: Code from Lesson 2 of iOS Track
    A sample Pokemon app with two views:
    - table view for all Pokemons
    - detail view for each Pokemon
    
    Hardcoded data.

---
+++ ios-track/lesson-2/Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Anton Sankov on 5/13/20.
+//  Copyright © 2020 Anton Sankov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    let pokemons = [
+        Pokemon(name: "Bulbasour", number: 1),
+        Pokemon(name: "Ivysaur", number: 2),
+        Pokemon(name: "Venusaur", number: 3)
+    ]
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemons.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell", for: indexPath)
+        cell.textLabel?.text = pokemons[indexPath.row].name
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PokemonSegue" {
+            if let destination = segue.destination as? PokemonViewController {
+                destination.pokemon = pokemons[tableView.indexPathForSelectedRow!.row]
+            }
+        }
+    }
+
+
+}
+
---
+++ ios-track/lesson-2/README.md
@@ -0,0 +1,9 @@
+### iOS Track - Lesson 2
+
+In this lesson we build on our Swift knowledge, by building a simple Pokemon Swift app with two views:
+- a list view that lists all Pokemons
+- a detail view that show details for each Pokemon (details being the name of the Pokemon and a number)
+
+![list-view](screenshots/listView.png)
+![detail-view](screenshots/detailView.png)
+
