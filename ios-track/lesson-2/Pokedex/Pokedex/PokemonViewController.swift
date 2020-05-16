d0febf6b0c8906df433b6a7dc9cc708c204a3b50
asankov <a.sankov96@gmail.com>
2020-05-17 01:29:29 +0300

    iOS Track: Pokedex
    Submit Catching part of Pokedex assignment - https://cs50.harvard.edu/x/2020/tracks/mobile/ios/pokedex/#catching

--- ios-track/lesson-2/Pokedex/Pokedex/PokemonViewController.swift
+++ ios-track/lesson-2/Pokedex/Pokedex/PokemonViewController.swift
@@ -13,13 +13,16 @@ class PokemonViewController: UIViewController {
     @IBOutlet var numberLabel: UILabel!
     @IBOutlet var type1Label: UILabel!
     @IBOutlet var type2Label: UILabel!
+    @IBOutlet var catchButton: UIButton!
     var pokemon: Pokemon!
-    
+    var pokemonCaught: Bool = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         type1Label.text = ""
         type2Label.text = ""
+        setTitle()
         
         let url = URL(string: pokemon.url)
         guard let u = url else {
@@ -51,5 +54,18 @@ class PokemonViewController: UIViewController {
                 print("\(error)")
             }
             }).resume()
+    }
+    
+    @IBAction func toggleCatch() {
+        pokemonCaught = !pokemonCaught
+        setTitle()
+    }
+    
+    func setTitle() {
+        if pokemonCaught {
+            catchButton.setTitle("Release", for: UIControl.State.normal)
+        } else {
+            catchButton.setTitle("Catch", for: UIControl.State.normal)
+        }
     }
 }
