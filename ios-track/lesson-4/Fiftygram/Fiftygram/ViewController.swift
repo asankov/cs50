61af7a47e5cc7aec515f4347a12dac8199c038dc
asankov <a.sankov96@gmail.com>
2020-05-18 00:00:41 +0300

    iOS Track: Lesson 4: Submit code from lesson 4

---
+++ ios-track/lesson-4/Fiftygram/Fiftygram/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Fiftygram
+//
+//  Created by Anton Sankov on 5/17/20.
+//  Copyright © 2020 Anton Sankov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    let context = CIContext()
+    var original: UIImage?
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBAction func choosePhoto() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .photoLibrary
+            navigationController?.present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func applySepia() {
+        if original == nil {
+            return
+        }
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+        display(filter: filter!)
+    }
+    
+    @IBAction func applyNoir() {
+        if original == nil {
+            return
+        }
+        let filter = CIFilter(name: "CIPhotoEffectNoir")
+        display(filter: filter!)
+    }
+    
+    @IBAction func applyVintage() {
+        if original == nil {
+            return
+        }
+        let filter = CIFilter(name: "CIPhotoEffectProcess")
+        display(filter: filter!)
+    }
+    
+    func display(filter: CIFilter) {
+        filter.setValue(CIImage(image: original!), forKey: kCIInputImageKey)
+        let output = filter.outputImage
+        imageView.image = UIImage(cgImage: self.context.createCGImage(output!, from: output!.extent)!)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        navigationController?.dismiss(animated: true, completion: nil)
+        if let image = info[.originalImage] as? UIImage {
+            imageView.image = image
+            original = image
+        }
+    }
+    
+}
+
---
+++ ios-track/lesson-4/README.md
@@ -0,0 +1,8 @@
+### iOS Track - Lesson 4
+
+In this lesson we started building an entirely new app - Fiftygram
+
+It allows the user to import a photo from the photo gallery and apply filters on it.
+
+The code can be found in [Fiftygram](Fiftygram)
+
